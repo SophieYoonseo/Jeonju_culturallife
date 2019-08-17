34027c8ec683fc45b5bf6e60850cf39bc8a8901d
김준성 <anima94@naver.com>
2019-08-17 22:04:52 +0900

    test_시행에는 manifest intent변경

---
+++ junseong/button_test/app/src/main/java/csejeonju2019/go/kr/button_test/MainActivity.java
@@ -0,0 +1,33 @@
+package csejeonju2019.go.kr.button_test;
+
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button); /*페이지 전환버튼*/
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), mapactivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
