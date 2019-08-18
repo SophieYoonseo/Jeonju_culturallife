ba8726f55499fa45f7f754091c285f862cfc1aeb
김준성 <anima94@naver.com>
2019-08-18 21:18:02 +0900

    이미지버튼 성공

--- junseong/button_test/app/src/main/java/csejeonju2019/go/kr/button_test/MainActivity.java
+++ junseong/button_test/app/src/main/java/csejeonju2019/go/kr/button_test/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -20,11 +21,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = (Button) findViewById(R.id.button); /*페이지 전환버튼*/
+        ImageButton button = (ImageButton) findViewById(R.id.button); /*페이지 전환버튼*/
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                Intent intent=new Intent(MainActivity.this,mapactivity.class);
                 startActivity(intent);
             }
 
