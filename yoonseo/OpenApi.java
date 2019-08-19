446e9e8c49a82700a1a958020c10b01d3fa93d19
SophieYoonseo <45335586+SophieYoonseo@users.noreply.github.com>
2019-08-19 20:58:08 +0900

    openapi test

---
+++ yoonseo/OpenApi.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class OpenApi {
+    public static void main(String[] args) {
+        BufferedReader br = null;
+        try{
+            String urlstr = "http://openapi.jeonju.go.kr/rest/experience/getExperienceList?" +
+                    "authApiKey=ro%2FXNFjTiuaWfUUOn939KiFkMvs0z915H%2BkR0Te9JF0NPfG4EjF9sAxR2%2B4%2FcqOzu9XlvQaZYyG0F4PniGNdsw%3D%3D" +
+                    "&dataValue=%EC%98%88%EB%8B%A4%EC%9B%90";
+            URL url = new URL(urlstr);
+            HttpURLConnection urlconnection = (HttpURLConnection) url.openConnection();
+            urlconnection.setRequestMethod("GET");
+            br = new BufferedReader(new InputStreamReader(urlconnection.getInputStream(),"UTF-8"));
+            String result = "";
+            String line;
+            while((line = br.readLine()) != null) {
+                result = result + line + "\n";
+            }
+            System.out.println(result);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+
