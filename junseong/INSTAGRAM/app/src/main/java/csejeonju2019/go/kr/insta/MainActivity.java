15777743e04bed10b3a084dc0987547d7d6ae9fd
김준성 <anima94@naver.com>
2019-08-19 16:24:29 +0900

    update and success

--- junseong/INSTAGRAM/app/src/main/java/csejeonju2019/go/kr/insta/MainActivity.java
+++ junseong/INSTAGRAM/app/src/main/java/csejeonju2019/go/kr/insta/MainActivity.java
@@ -1,5 +1,5 @@
 package csejeonju2019.go.kr.insta;
-
+import android.support.v4.app.Fragment;
 import android.support.annotation.NonNull;
 import android.support.design.internal.BottomNavigationItemView;
 import android.support.design.widget.BottomNavigationView;
