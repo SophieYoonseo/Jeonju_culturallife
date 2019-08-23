9c9e346f2c15a7f87b8716766534e61b75a35aec
정종범 <danieltiger60@gmail.com>
2019-08-23 17:29:57 +0900

    AfterActivity 개발중

--- jongbum/naver/app/src/main/java/kr/go/csejeonju2019/MainActivity.java
+++ jongbum/naver/app/src/main/java/kr/go/csejeonju2019/MainActivity.java
@@ -2,9 +2,13 @@ package kr.go.csejeonju2019;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.nhn.android.naverlogin.OAuthLogin;
+import com.nhn.android.naverlogin.OAuthLoginHandler;
 import com.nhn.android.naverlogin.ui.view.OAuthLoginButton;
 import static com.nhn.android.naverlogin.OAuthLogin.mOAuthLoginHandler;
 
@@ -12,6 +16,28 @@ import static com.nhn.android.naverlogin.OAuthLogin.mOAuthLoginHandler;
 public class MainActivity extends AppCompatActivity {
     OAuthLogin mOAuthLoginModule;
     OAuthLoginButton mOAuthLoginButton;
+    Context mContext;
+    private OAuthLoginHandler mOAuthLoginHandler = new OAuthLoginHandler() {
+        @Override
+        public void run(boolean success) {
+            if (success) {
+                String accessToken = mOAuthLoginModule.getAccessToken(mContext);
+                String refreshToken = mOAuthLoginModule.getRefreshToken(mContext);
+                long expiresAt = mOAuthLoginModule.getExpiresAt(mContext);
+                String tokenType = mOAuthLoginModule.getTokenType(mContext);
+                //Toast.makeText(getApplicationContext(), accessToken, Toast.LENGTH_LONG).show();
+                //startActivity(new Intent(getApplicationContext(), ))
+
+            } else {
+                String errorCode = mOAuthLoginModule.getLastErrorCode(mContext).getCode();
+                String errorDesc = mOAuthLoginModule.getLastErrorDesc(mContext);
+                Toast.makeText(mContext, "errorCode:" + errorCode
+                        + ", errorDesc:" + errorDesc, Toast.LENGTH_SHORT).show();
+            }
+        };
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,12 +45,18 @@ public class MainActivity extends AppCompatActivity {
         mOAuthLoginModule = OAuthLogin.getInstance();
         mOAuthLoginModule.init(
                 MainActivity.this
-                ,"0PSGjaLXb_f6WljImx1S"
-                ,"7mzm8tggH6"
-                ,"CSEJEONJU"
+                , "0PSGjaLXb_f6WljImx1S"
+                , "7mzm8tggH6"
+                , "CSEJEONJU"
                 //,OAUTH_CALLBACK_INTENT
                 // SDK 4.1.4 버전부터는 OAUTH_CALLBACK_INTENT변수를 사용하지 않습니다.
         );
+        mOAuthLoginButton = findViewById(R.id.buttonOAuthLoginImg);
+        mOAuthLoginButton.setOAuthLoginHandler(mOAuthLoginHandler);
+        //mOAuthLoginButton.setBgResourceId(R.drawable.img_loginbtn_usercustom);
+        //mOAuthLoginModule.startOauthLoginActivity(this, mOAuthLoginHandler);
+    }
 
-}
+
+
 }
