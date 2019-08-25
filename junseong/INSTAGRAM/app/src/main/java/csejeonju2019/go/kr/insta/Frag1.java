c38de85a266d49d34bd9c196cb5f67e042ae6d33
김준성 <anima94@naver.com>
2019-08-25 16:50:06 +0900

    thread문제 해결실패

--- junseong/INSTAGRAM/app/src/main/java/csejeonju2019/go/kr/insta/Frag1.java
+++ junseong/INSTAGRAM/app/src/main/java/csejeonju2019/go/kr/insta/Frag1.java
@@ -1,73 +1,35 @@
 package csejeonju2019.go.kr.insta;
 
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.maps.MapView;
 
-import me.relex.circleindicator.CircleIndicator;
 
-public class Frag1 extends Fragment {
-    FragmentPagerAdapter adapterViewPager;
+public class Frag1 extends Fragment
+{
+
     public Frag1(){
 
     }
-
+    ViewPager viewPager;
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View layout = inflater.inflate(R.layout.frag1, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v=inflater.inflate(R.layout.frag1,container,false);
+        viewPager=v.findViewById(R.id.viewPager);
 
-
-        return layout;
-    }
-    public static class MyPagerAdapter extends FragmentPagerAdapter {
-        private static int NUM_ITEMS = 3;
-
-        public MyPagerAdapter(FragmentManager fragmentManager) {
-            super(fragmentManager);
-        }
-
-        // Returns total number of pages
-        @Override
-        public int getCount() {
-            return NUM_ITEMS;
-        }
-
-        // Returns the fragment to display for that page
-        @Override
-
-        public Fragment getItem(int position) {
-            switch (position) {
-                case 0:
-
-                    return FirstFragment.newInstance(0, "Page # 1");
-                case 1:
-                    return SecondFragment.newInstance(1, "Page # 2");
-                case 2:
-                    return ThirdFragment.newInstance(2, "Page # 3");
-                default:
-                    return null;
-            }
-        }
-
-        // Returns the page title for the top indicator
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return "Page " + position;
-        }
-
+        return viewPager;
     }
 }
 
