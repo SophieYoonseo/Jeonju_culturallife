27ea28a31077459efd5df24e7c7f61211a2ffee7
mrJangJinSeong <1322ijin@daum.net>
2019-08-26 00:09:55 +0900

    KakaoLoginTest_0825_실패

---
+++ MrJang/KakaoLoginTest/KakaoLoginTest_0825_02/app/src/main/java/kr/go/csejeonju2019/SampleLoginActivity.java
@@ -0,0 +1,65 @@
+package kr.go.csejeonju2019;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.kakao.auth.ISessionCallback;
+import com.kakao.auth.Session;
+import com.kakao.util.exception.KakaoException;
+import com.kakao.util.helper.log.Logger;
+
+public class SampleLoginActivity extends Activity {
+    private SessionCallback callback;
+
+    /**
+     * 로그인 버튼을 클릭 했을시 access token을 요청하도록 설정한다.
+     *
+     * @param savedInstanceState 기존 session 정보가 저장된 객체
+     */
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        callback = new SessionCallback();
+        Session.getCurrentSession().addCallback(callback);
+        Session.getCurrentSession().checkAndImplicitOpen();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
+            return;
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Session.getCurrentSession().removeCallback(callback);
+    }
+
+    private class SessionCallback implements ISessionCallback {
+
+        @Override
+        public void onSessionOpened() {
+            redirectSignupActivity();
+        }
+
+        @Override
+        public void onSessionOpenFailed(KakaoException exception) {
+            if(exception != null) {
+                Logger.e(exception);
+            }
+        }
+    }
+
+    protected void redirectSignupActivity() {
+        final Intent intent = new Intent(this, SampleSignupActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
