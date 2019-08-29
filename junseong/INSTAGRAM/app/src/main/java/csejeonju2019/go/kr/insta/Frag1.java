cf028fc7ff945e43500aff1a1d32e84be2f5e8f4
김준성 <anima94@naver.com>
2019-08-29 10:33:46 +0900

    revert용

--- junseong/INSTAGRAM/app/src/main/java/csejeonju2019/go/kr/insta/Frag1.java
+++ junseong/INSTAGRAM/app/src/main/java/csejeonju2019/go/kr/insta/Frag1.java
@@ -3,25 +3,18 @@ package csejeonju2019.go.kr.insta;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.ImageView;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+
+
 
 public class Frag1 extends Fragment
 {
-    int i=0;
-    ImageView imageView=null;
-
-
+    ViewPager viewPager;
+    private Object ViewPagerAdapter;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,22 +26,9 @@ public class Frag1 extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.frag1, container, false);
-        ImageButton button=(ImageButton)view.findViewById(R.id.button);
-        imageView=(ImageView)view.findViewById(R.id.imageView);
-        button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                i=1-i;
-                if(i==0){
-                    imageView.setImageResource(R.drawable.slide2);
-                }
-                else{
-                    imageView.setImageResource(R.drawable.slide3);
-                }
-
-            }
-        });
-
+        ViewPagerAdapter=(ViewPager)view.findViewById(R.id.viewPager);
+         ViewPagerAdapter viewPagerAdapter=new ViewPagerAdapter();
+         viewPager.setAdapter(viewPagerAdapter);
         return view;
     }
 
