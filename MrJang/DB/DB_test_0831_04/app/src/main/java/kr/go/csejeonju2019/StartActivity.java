90c00e9afd07b26aba13f2b30e2fe2f4ae2ed0b5
mrJangJinSeong <1322ijin@daum.net>
2019-09-01 00:02:10 +0900

    DB 예제 실습, 실패.

---
+++ MrJang/DB/DB_test_0831_04/app/src/main/java/kr/go/csejeonju2019/StartActivity.java
@@ -0,0 +1,93 @@
+package kr.go.csejeonju2019;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class StartActivity extends AppCompatActivity {
+
+    private EditText user_chat, user_edit;
+    private Button user_next;
+    private ListView chat_list;
+
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        user_chat = (EditText) findViewById(R.id.user_chat);
+        user_edit = (EditText) findViewById(R.id.user_edit);
+        user_next = (Button) findViewById(R.id.user_next);
+        chat_list = (ListView) findViewById(R.id.chat_list);
+
+        user_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (user_edit.getText().toString().equals("") || user_chat.getText().toString().equals(""))
+                    return;
+
+                Intent intent = new Intent(StartActivity.this, ChatActivity.class);
+                intent.putExtra("chatName", user_chat.getText().toString());
+                intent.putExtra("userName", user_edit.getText().toString());
+                startActivity(intent);
+            }
+        });
+
+        showChatList();
+
+    }
+
+    private void showChatList() {
+        // 리스트 어댑터 생성 및 세팅
+        final ArrayAdapter<String> adapter
+
+                = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1);
+        chat_list.setAdapter(adapter);
+
+        // 데이터 받아오기 및 어댑터 데이터 추가 및 삭제 등..리스너 관리
+        databaseReference.child("chat").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Log.e("LOG", "dataSnapshot.getKey() : " + dataSnapshot.getKey());
+                adapter.add(dataSnapshot.getKey());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+}
