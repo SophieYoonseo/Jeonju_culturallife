940777551bffe813e4d463e672daf7dc34744ad6
saechimdaeki <anima94@naver.com>
2019-09-03 21:18:13 +0900

    클래스 왕창추가및 조나단과천우희 (윤서쿤이 어댑터및연결 함)
    Co-Authored-By: sophieyoonseo <sophieyoonseo@users.noreply.github.com>

---
+++ junseong/INSTAGRAM/app/src/main/java/csejeonju2019/go/kr/insta/SecondTwoFragment.java
@@ -0,0 +1,37 @@
+package csejeonju2019.go.kr.insta;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SecondTwoFragment  extends Fragment {
+    // Store instance variables
+
+
+    // Store instance variables based on arguments passed
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    // Inflate the view for the fragment based on layout XML
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.secondtwo_fragment, container, false);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Intent.ACTION_VIEW, Uri.parse("https://namu.wiki/w/%EB%A9%9C%EB%A1%9C%EA%B0%80%20%EC%B2%B4%EC%A7%88"));
+                startActivity(intent);
+            }
+        });
+
+        return view;
+    }
+}
