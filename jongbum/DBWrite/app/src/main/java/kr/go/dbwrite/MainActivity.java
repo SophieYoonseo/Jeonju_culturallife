ca4ebe0577310213482589c1d837b27de9baa70d
정종범 <danieltiger60@gmail.com>
2019-09-06 17:38:53 +0900

    DBWrite(Realtime database) 완료
    추후 Firebase credential 변경 필요

--- jongbum/DBWrite/app/src/main/java/kr/go/dbwrite/MainActivity.java
+++ jongbum/DBWrite/app/src/main/java/kr/go/dbwrite/MainActivity.java
@@ -3,12 +3,27 @@ package kr.go.dbwrite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button)findViewById(R.id.button);
+
+        btn.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v) {
+                databaseReference.child("message").push().setValue("111111");
+            }
+        });
     }
+
 }
