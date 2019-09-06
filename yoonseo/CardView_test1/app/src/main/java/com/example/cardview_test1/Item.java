1961d624ab0a87a89a8ed0c1f9f94087e80a2643
SophieYoonseo <45335586+SophieYoonseo@users.noreply.github.com>
2019-09-06 19:21:48 +0900

    카드뷰예제

--- yoonseo/CardView_test1/app/src/main/java/com/example/cardview_test1/Item.java
+++
@@ -1,7 +0,0 @@
-package com.example.cardview_test1;
-
-
-public class Item {
-    String title;
-    String description;
-}
--- yoonseo/CardView_test1/app/src/main/java/com/example/cardview_test1/MainActivity.java
+++ yoonseo/CardView_test1/app/src/main/java/com/example/cardview_test1/MainActivity.java
@@ -1,28 +1,48 @@
 package com.example.cardview_test1;
 
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import android.support.widget.LinearLayoutManager;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
-    @BindView(R.id.recycler_view)
-    RecyclerView mRecyclerView;
+public class MainActivity extends Activity {
+
+    private RecyclerView rv;
+    private LinearLayoutManager llm;
+    private List<Integer> count;
+    private int i = 0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
 
-        ItemRepository itemRepository = new ItemRepository();
+        rv = (RecyclerView) findViewById(R.id.main_rv);
+        llm = new LinearLayoutManager(this);
 
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-        RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(getApplicationContext(), itemRepository.getAllItems());
-        mRecyclerView.setAdapter(recyclerViewAdapter);
+        count = new ArrayList<>();
 
+        rv.setHasFixedSize(true);
+        rv.setLayoutManager(llm);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.main_btn_add : {
+                i ++;
+                count.add(i);
+                RvAdapter adapter = new RvAdapter(getApplication(), count, i);
+                rv.setAdapter(adapter);
+                Log.d("Count", count + "");
+                break;
+            }
+        }
     }
 }
