191178a9574d81284f7cc2225d9d01bef52990e9
정종범 <danieltiger60@gmail.com>
2019-09-06 18:23:26 +0900

    DB write 작업
    firebase console에 띄우기 성공

--- jongbum/DBWrite/app/src/main/java/kr/go/dbwrite/MainActivity.java
+++ jongbum/DBWrite/app/src/main/java/kr/go/dbwrite/MainActivity.java
@@ -2,17 +2,46 @@ package kr.go.dbwrite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.nhn.android.naverlogin.OAuthLogin;
+import com.nhn.android.naverlogin.OAuthLoginHandler;
+import com.nhn.android.naverlogin.ui.view.OAuthLoginButton;
+
+import static com.nhn.android.naverlogin.OAuthLogin.mOAuthLoginHandler;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     private DatabaseReference databaseReference = firebaseDatabase.getReference();
     private Button btn;
+    OAuthLogin mOAuthLoginModule;
+    OAuthLoginButton mOAuthLoginButton;
+    Context mContext;
+    private OAuthLoginHandler mOAuthLoginHandler = new OAuthLoginHandler() {
+        @Override
+        public void run(boolean success) {
+            if (success) {
+                String accessToken = mOAuthLoginModule.getAccessToken(mContext);
+                String refreshToken = mOAuthLoginModule.getRefreshToken(mContext);
+                long expiresAt = mOAuthLoginModule.getExpiresAt(mContext);
+                String tokenType = mOAuthLoginModule.getTokenType(mContext);
+                //성공시 처리
+                databaseReference.child("AccessToken").push().setValue(accessToken);
+
+            } else {
+                String errorCode = mOAuthLoginModule.getLastErrorCode(mContext).getCode();
+                String errorDesc = mOAuthLoginModule.getLastErrorDesc(mContext);
+                Toast.makeText(mContext, "errorCode:" + errorCode
+                        + ", errorDesc:" + errorDesc, Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +53,22 @@ public class MainActivity extends AppCompatActivity {
                 databaseReference.child("message").push().setValue("111111");
             }
         });
+        mOAuthLoginModule = OAuthLogin.getInstance();
+        mOAuthLoginModule.init(
+                MainActivity.this
+                ,"0PSGjaLXb_f6WljImx1S"
+                ,"7mzm8tggH6"
+                ,"cseJeonju2019"
+                //,OAUTH_CALLBACK_INTENT
+                // SDK 4.1.4 버전부터는 OAUTH_CALLBACK_INTENT변수를 사용하지 않습니다.
+        );
+        mOAuthLoginButton = (OAuthLoginButton) findViewById(R.id.buttonOAuthLoginImg);
+        mOAuthLoginButton.setOAuthLoginHandler(mOAuthLoginHandler);
     }
-
+    public void onDestroy(){
+        mOAuthLoginModule.logout(mContext);
+        //mGoogleSignInClient.signOut();
+        Toast.makeText(MainActivity.this, "로그아웃되었습니다", Toast.LENGTH_LONG).show();
+        super.onDestroy();
+    }
 }
