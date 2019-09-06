529f0df7cd3286f8cbb36a5fc35015c59a2a2511
정종범 <danieltiger60@gmail.com>
2019-09-07 01:10:11 +0900

    Google, Naver 파이어베이스 연동 작업중

--- jongbum/DBWrite/app/src/main/java/kr/go/dbwrite/MainActivity.java
+++ jongbum/DBWrite/app/src/main/java/kr/go/dbwrite/MainActivity.java
@@ -1,18 +1,32 @@
 package kr.go.dbwrite;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.nhn.android.naverlogin.OAuthLogin;
 import com.nhn.android.naverlogin.OAuthLoginHandler;
 import com.nhn.android.naverlogin.ui.view.OAuthLoginButton;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static com.nhn.android.naverlogin.OAuthLogin.mOAuthLoginHandler;
 
@@ -23,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
     OAuthLogin mOAuthLoginModule;
     OAuthLoginButton mOAuthLoginButton;
     Context mContext;
+    GoogleSignInClient mGoogleSignInClient;
+    SignInButton signInButton;
+    public static final int REQUEST_CODE_MENU = 101, RC_SIGN_IN = 100;
     private OAuthLoginHandler mOAuthLoginHandler = new OAuthLoginHandler() {
         @Override
         public void run(boolean success) {
@@ -32,7 +49,9 @@ public class MainActivity extends AppCompatActivity {
                 long expiresAt = mOAuthLoginModule.getExpiresAt(mContext);
                 String tokenType = mOAuthLoginModule.getTokenType(mContext);
                 //성공시 처리
-                databaseReference.child("AccessToken").push().setValue(accessToken);
+                databaseReference.child("Account").child("AccessToken").push().setValue(accessToken);
+                Date mDate = new Date(System.currentTimeMillis());
+                databaseReference.child("Account").child("AccessTime").push().setValue((new SimpleDateFormat("yyyy-MM-dd hh:mm:ss")).format(mDate));
 
             } else {
                 String errorCode = mOAuthLoginModule.getLastErrorCode(mContext).getCode();
@@ -42,17 +61,64 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            //updateUI(account);
+            Toast.makeText(this, "Google Login Successful!", Toast.LENGTH_LONG).show();
+            GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+            if (acct != null) {
+                String personName = acct.getDisplayName();
+                String personGivenName = acct.getGivenName();
+                String personFamilyName = acct.getFamilyName();
+                String personEmail = acct.getEmail();
+                String personId = acct.getId();
+                Uri personPhoto = acct.getPhotoUrl();
+                //Toast.makeText(this, personId, Toast.LENGTH_LONG).show();
+                //StartActivityForResult(new Intent(getApplicationContext(), AfterActivity.class), REQUEST_CODE_MENU);
+                databaseReference.child("Account").child("AccessToken").push().setValue(personEmail);
+                Date mDate = new Date(System.currentTimeMillis());
+                databaseReference.child("Account").child("AccessTime").push().setValue((new SimpleDateFormat("yyyy-MM-dd hh:mm:ss")).format(mDate));
+            }
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("TAG", "signInResult:failed code=" + e.getStatusCode());
+            //updateUI(null);
+            Toast.makeText(this, "Google Login Fail!", Toast.LENGTH_LONG).show();
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn = (Button)findViewById(R.id.button);
 
-        btn.setOnClickListener(new Button.OnClickListener(){
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_WIDE);
+        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                databaseReference.child("message").push().setValue("111111");
+                switch (v.getId()) {
+                    case R.id.sign_in_button:
+                        signIn();
+                        break;
+                }
+            }
+            private void signIn() {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
             }
         });
+
         mOAuthLoginModule = OAuthLogin.getInstance();
         mOAuthLoginModule.init(
                 MainActivity.this
@@ -68,7 +134,17 @@ public class MainActivity extends AppCompatActivity {
     public void onDestroy(){
         mOAuthLoginModule.logout(mContext);
         //mGoogleSignInClient.signOut();
-        Toast.makeText(MainActivity.this, "로그아웃되었습니다", Toast.LENGTH_LONG).show();
         super.onDestroy();
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+
     }
 }
