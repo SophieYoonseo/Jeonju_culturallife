0273a650654edd1f699a89446dd1593dc102e647
김준성 <anima94@naver.com>
2019-09-14 00:41:44 +0900

    test

--- INSTAGRAM/app/src/main/java/csejeonju2019/go/kr/insta/Frag3.java
+++ INSTAGRAM/app/src/main/java/csejeonju2019/go/kr/insta/Frag3.java
@@ -16,7 +16,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Frag3 extends Fragment implements OnMapReadyCallback
+public class Frag3 extends Fragment
 {
     public double loc1,loc2;
 
@@ -39,69 +39,61 @@ public class Frag3 extends Fragment implements OnMapReadyCallback
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View layout = inflater.inflate(R.layout.frag3, container, false);
         Button button=(Button)layout.findViewById(R.id.button_share);
-        mapView = (MapView)layout.findViewById(R.id.map);
-        mapView.getMapAsync(this);
-        button.setOnClickListener(new View.OnClickListener(){
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String mySharedLink = "https://github.com/SophieYoonseo/Jeonju_culturallife/tree/master/junseong";
-                String mySubject = "오픈소스로 초대합니덩.";
-
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_SEND);
-                intent.setType("text/plain");
-                intent.putExtra(Intent.EXTRA_SUBJECT, mySubject);
-                intent.putExtra(Intent.EXTRA_TEXT, mySharedLink);
-
-                startActivity(Intent.createChooser(intent, "공유합니다."));
-
-
-
+                Intent intent=new Intent(getActivity(),signup.class);
+                startActivity(intent);
             }
         });
+
+
+
+
+
         return layout;
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        mapView.onStart();
+
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        mapView.onStop();
+
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        mapView.onSaveInstanceState(outState);
+
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        mapView.onResume();
+
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        mapView.onPause();
+
     }
 
     @Override
     public void onLowMemory() {
         super.onLowMemory();
-        mapView.onLowMemory();
+
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        mapView.onLowMemory();
+
     }
 
     @Override
@@ -110,24 +102,10 @@ public class Frag3 extends Fragment implements OnMapReadyCallback
 
         //액티비티가 처음 생성될 때 실행되는 함수
 
-        if(mapView != null)
-        {
-            mapView.onCreate(savedInstanceState);
-        }
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        LatLng jeonju = new LatLng(35.846925, 127.129456);
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(jeonju);
-        markerOptions.title("전북대학교");
-        markerOptions.snippet("전북의 하바드");
-        googleMap.addMarker(markerOptions);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(jeonju,16));//16배 줌가능
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(jeonju,16));//지도 시작시 애니메이션 효과
 
     }
+
+
 
 }
 
---
+++ INSTAGRAM/app/src/main/java/csejeonju2019/go/kr/insta/signup.java
@@ -0,0 +1,122 @@
+package csejeonju2019.go.kr.insta;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+//import androidx.annotation.NonNull;
+//import androidx.annotation.Nullable;
+//import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+public class signup extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
+
+    GoogleApiClient mGoogleApiClient;
+    int RC_SIGN_IN = 1000;
+    String TAG = "TAG";
+    TextView mStatusTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */,  this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.setScopes(gso.getScopeArray());
+
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+        mStatusTextView= (TextView) findViewById(R.id.textView);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.sign_in_button:
+                signIn();
+                break;
+        }
+    }
+
+    private void signIn(){
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }else{
+
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            //mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
+            updateUI(true);
+        } else {
+            // Signed out, show unauthenticated UI.
+            updateUI(false);
+        }
+    }
+    private void updateUI(boolean signedIn) {
+        if (signedIn) {
+            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+            mStatusTextView.setText("로그인 성공!!");
+        } else {
+            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+            mStatusTextView.setText("로그인 실패!!");
+        }
+    }
+
+
+
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(getApplicationContext(), ""+connectionResult, Toast.LENGTH_SHORT).show();
+    }
+}
+
+
