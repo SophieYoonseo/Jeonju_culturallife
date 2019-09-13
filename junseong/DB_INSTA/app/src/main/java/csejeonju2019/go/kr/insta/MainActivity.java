5b217250a025405b1ab9e363c61f75f520298ec3
김준성 <anima94@naver.com>
2019-09-13 13:45:24 +0900

    firebase google login successs

--- junseong/DB_INSTA/app/src/main/java/csejeonju2019/go/kr/insta/MainActivity.java
+++ junseong/DB_INSTA/app/src/main/java/csejeonju2019/go/kr/insta/MainActivity.java
@@ -1,14 +1,130 @@
 package csejeonju2019.go.kr.insta;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG ="MainActivity" ;
+    final int RC_SIGN_IN = 1001; // 로그인 확인여부 코드
+    private FirebaseAuth mAuth;
+    private SignInButton signInButton; //구글 로그인 버튼
+    private GoogleApiClient mGoogleApiClient; //API 클라이언트
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance(); // 인스턴스 생성
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        signInButton = (SignInButton)findViewById(R.id.Google_Login);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        });
+
     }
-}
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+
+                Log.d(TAG, "이름 =" + account.getDisplayName());
+                Log.d(TAG, "이메일=" + account.getEmail());
+                Log.d(TAG, "getId()=" + account.getId());
+                Log.d(TAG, "getAccount()=" + account.getAccount());
+                Log.d(TAG, "getIdToken()=" + account.getIdToken());
+                firebaseAuthWithGoogle(account);
+            } else {
+                // Google Sign In failed, update UI appropriately
+                // ...
+            }
+        }
+    }
+
+    public void onStart() { // 사용자가 현재 로그인되어 있는지 확인
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser!=null){ // 만약 로그인이 되어있으면 다음 액티비티 실행
+            Intent intent = new Intent(getApplicationContext(), signup.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Intent intent = new Intent(getApplicationContext(), signup.class);
+                            startActivity(intent);
+                            finish();
+                            Log.d(TAG, "signInWithCredential:success");
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+    }
--- junseong/DB_INSTA/app/src/main/java/csejeonju2019/go/kr/insta/signup.java
+++ junseong/DB_INSTA/app/src/main/java/csejeonju2019/go/kr/insta/signup.java
@@ -22,53 +22,9 @@ public class signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
-        mAuth=FirebaseAuth.getInstance();
-        findViewById(R.id.sign_up_button).setOnClickListener(onClickListener);
-    }
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
+
 
     }
 
-    View.OnClickListener onClickListener=new View.OnClickListener(){
-        public void onClick(View v){
-            switch (v.getId()){
-                case R.id.sign_up_button:
-                    sign();
-                    Log.e("클릭","클릭");
-                    break;
-            }
-        }
-    };
 
-    private void sign(){
-        String email=((EditText)findViewById(R.id.email_text)).getText().toString();
-        String password=((EditText)findViewById(R.id.password_text)).getText().toString();
-
-
-
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                           // updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                           // Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
-                             //       Toast.LENGTH_SHORT).show();
-                         //   updateUI(null);
-                        }
-
-                        // ...
-                    }
-                });
-    }
 }
