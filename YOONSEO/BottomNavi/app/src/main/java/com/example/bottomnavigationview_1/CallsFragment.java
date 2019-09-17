9dcf12b5fed6ae21ddb48ce197952460449cdc63
SophieYoonseo <45335586+SophieYoonseo@users.noreply.github.com>
2019-09-17 19:42:06 +0900

    bottomNavigation 예제

---
+++ YOONSEO/BottomNavi/app/src/main/java/com/example/bottomnavigationview_1/CallsFragment.java
@@ -0,0 +1,23 @@
+package com.example.bottomnavigationview_1;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class CallsFragment extends Fragment {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_calls, container, false);
+
+        return view;
+    }
+}
