dc5ff97e822abdfa6ba40314e313a208b5cfaca7
mrJangJinSeong <1322ijin@daum.net>
2019-09-19 23:54:53 +0900

    KakaoLogin 재구현중

--- MrJang/KakaoLoginTest/KakaoLoginTest_0918_04/app/src/main/java/kr/go/csejeonju2019/MainActivity.java
+++ MrJang/KakaoLoginTest/KakaoLoginTest_0918_04/app/src/main/java/kr/go/csejeonju2019/MainActivity.java
@@ -1,8 +1,54 @@
 package kr.go.csejeonju2019;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.kakao.auth.AuthType;
+import com.kakao.auth.Session;
+
+
+import android.content.Context;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private Context mContext;
+    private Button btn_custom_login;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mContext = getApplicationContext();
+
+
+
+        btn_custom_login = (Button) findViewById(R.id.btn_custom_login);
+
+        btn_custom_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Session session = Session.getCurrentSession();
+                session.addCallback(new SessionCallback());
+                session.open(AuthType.KAKAO_LOGIN_ALL, MainActivity.this);
+            }
+        });
+    }
 
 }
+
+
+/*
+
+
+
+
+public class LoginActivity extends AppCompatActivity
+
+
+
+ */
