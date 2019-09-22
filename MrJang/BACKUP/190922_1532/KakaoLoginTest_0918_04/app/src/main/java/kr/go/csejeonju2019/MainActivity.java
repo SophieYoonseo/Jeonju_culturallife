da6e6861ab6fcb69cadc210bd83ca15109169eec
mrJangJinSeong <1322ijin@daum.net>
2019-09-22 22:12:10 +0900

    back_up_파일생성

---
+++ MrJang/BACKUP/190922_1532/KakaoLoginTest_0918_04/app/src/main/java/kr/go/csejeonju2019/MainActivity.java
@@ -0,0 +1,23 @@
+package kr.go.csejeonju2019;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.kakao.auth.AuthType;
+import com.kakao.auth.Session;
+import android.content.Context;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mContext = getApplicationContext();
+
+        Session session = Session.getCurrentSession();
+        session.addCallback(new SessionCallback());
+        session.open(AuthType.KAKAO_LOGIN_ALL, MainActivity.this);
+    }
+}
