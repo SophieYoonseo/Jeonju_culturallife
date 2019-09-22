41bf420c85d63be6a2759980dc0f0c7553536438
mrJangJinSeong <1322ijin@daum.net>
2019-09-22 14:30:45 +0900

    main 수정

--- MrJang/KakaoLoginTest/KakaoLoginTest_0918_04/app/src/main/java/kr/go/csejeonju2019/MainActivity.java
+++ MrJang/KakaoLoginTest/KakaoLoginTest_0918_04/app/src/main/java/kr/go/csejeonju2019/MainActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.kakao.auth.AuthType;
 import com.kakao.auth.Session;
 import android.content.Context;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private Context mContext;
+    private Button mKakaoLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +18,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mContext = getApplicationContext();
 
+        /*
         //카카오 버튼 리스너, 알고보니, 최종구현
         Session session = Session.getCurrentSession();
         session.addCallback(new SessionCallback());
-        session.open(AuthType.KAKAO_LOGIN_ALL, MainActivity.this);
+        session.open(AuthType.KAKAO_ACCOUNT, MainActivity.this);
+         */
 
     }
 }
