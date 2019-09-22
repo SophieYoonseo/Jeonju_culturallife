a27daffd3025d1df3035413fa5115d192ff78a90
김준성 <anima94@naver.com>
2019-09-22 14:28:50 +0900

    오류수정

--- MrJang/KakaoLoginTest/KakaoLoginTest_0918_04/app/src/main/java/kr/go/csejeonju2019/MainActivity.java
+++ MrJang/KakaoLoginTest/KakaoLoginTest_0918_04/app/src/main/java/kr/go/csejeonju2019/MainActivity.java
@@ -1,10 +1,16 @@
 package kr.go.csejeonju2019;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import com.kakao.auth.AuthType;
 import com.kakao.auth.Session;
+import com.kakao.usermgmt.LoginButton;
+
 import android.content.Context;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,12 +20,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LoginButton button=(LoginButton)findViewById(R.id.btn_kakao_login);
         mContext = getApplicationContext();
 
-        //카카오 버튼 리스너, 알고보니, 최종구현
-        Session session = Session.getCurrentSession();
-        session.addCallback(new SessionCallback());
-        session.open(AuthType.KAKAO_LOGIN_ALL, MainActivity.this);
+        button.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
 
-    }
+                Session session = Session.getCurrentSession();
+                session.addCallback(new SessionCallback());
+                session.open(AuthType.KAKAO_LOGIN_ALL, MainActivity.this);
+
+            }
+        });
+
+
+
+
+        //카카오 버튼 리스너, 알고보니, 최종구현
+
+
+
+}
 }
