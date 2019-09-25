c82c9f330aad4c9303f6a127fdbb203e43268d08
SophieYoonseo <yyumz6961@gmail.com>
2019-09-25 21:14:08 +0900

    앱 아이콘 수정

--- INSTAGRAM/app/src/main/java/csejeonju2019/go/kr/insta/SplashActivity.java
+++ INSTAGRAM/app/src/main/java/csejeonju2019/go/kr/insta/SplashActivity.java
@@ -10,9 +10,6 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loading);
-        startLoading();
-    }
-    private void startLoading() {
         Handler handler=new Handler();
         handler.postDelayed(new Runnable() {
             @Override
