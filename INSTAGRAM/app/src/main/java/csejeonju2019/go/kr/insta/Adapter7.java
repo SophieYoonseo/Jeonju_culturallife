5ad439c14f55c751180595ce4daa1d5fa61da124
김준성 <anima94@naver.com>
2019-09-28 13:22:43 +0900

    Revert "Revert "revery""
    This reverts commit 72c3c2837a5b4c9f4d0995ee0b8dbf8e2343409c.

---
+++ INSTAGRAM/app/src/main/java/csejeonju2019/go/kr/insta/Adapter7.java
@@ -0,0 +1,101 @@
+package csejeonju2019.go.kr.insta;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.viewpager.widget.PagerAdapter;
+
+public class Adapter7 extends PagerAdapter {
+    private int[] images = {R.drawable.gaeklidan,
+            R.drawable.hanokmaul,
+            R.drawable.namboosizang,
+            R.drawable.poongnam,
+            R.drawable.hanokguest,
+
+
+
+
+    };
+    //경기전 → 어진박물관 → 태조로 → 오목대 → 이목대 → 자만벽화마을
+    String[] imagee={
+            "객리단길",
+            "전주한옥마을",
+            "남부시장",
+            "풍남문",
+            "한옥게스트하우스",
+
+
+
+
+
+
+    };
+    private LayoutInflater inflater;
+    private Context context;
+    public Adapter7(Context context){
+        this.context = context;
+    }
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == ((LinearLayout) object);
+    }
+    @Override
+    public Object instantiateItem(ViewGroup container, final int position) {
+        inflater = (LayoutInflater)context.getSystemService
+                (Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.slider7, container, false);
+        ImageView imageView = (ImageView)v.findViewById(R.id.imageView);
+        TextView textView = (TextView)v.findViewById(R.id.textView);
+        imageView.setImageResource(images[position]);
+        String text = imagee[position];
+        textView.setText(text);
+        container.addView(v);
+        v.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(position==0)
+                {
+                    Intent homepage=new Intent(Intent.ACTION_VIEW, Uri.parse("https://terms.naver.com/entry.nhn?docId=948171&cid=42865&categoryId=42865"));
+                    context.startActivity(homepage);
+                }
+                else  if(position==1)
+                {
+                    Intent homepage=new Intent(Intent.ACTION_VIEW, Uri.parse("https://store.naver.com/attractions/detail?entry=plt&id=19550901&query=%EC%96%B4%EC%A7%84%EB%B0%95%EB%AC%BC%EA%B4%80"));
+                    context.startActivity(homepage);
+                }  else if(position==2)
+                {
+                    Intent homepage=new Intent(Intent.ACTION_VIEW, Uri.parse("http://blog.naver.com/PostView.nhn?blogId=bada3347&logNo=220796100043&categoryNo=0&parentCategoryNo=0&viewDate=&currentPage=1&postListTopCurrentPage=1&from=postView"));
+                    context.startActivity(homepage);
+                } else if(position==3)
+                {
+                    Intent homepage=new Intent(Intent.ACTION_VIEW, Uri.parse("https://store.naver.com/attractions/detail?entry=plt&id=20103217&query=%EC%98%A4%EB%AA%A9%EB%8C%80"));
+                    context.startActivity(homepage);
+                } else if(position==4)
+                {
+                    Intent homepage=new Intent(Intent.ACTION_VIEW, Uri.parse("https://store.naver.com/restaurants/detail?entry=plt&id=35904979&query=%EC%9D%B4%EB%AA%A9%EB%8C%80"));
+                    context.startActivity(homepage);
+                }else if(position==5)
+                {
+                    Intent homepage=new Intent(Intent.ACTION_VIEW, Uri.parse("https://store.naver.com/attractions/detail?entry=plt&id=36439466&query=%EC%9E%90%EB%A7%8C%EB%B2%BD%ED%99%94%EB%A7%88%EC%9D%84"));
+                    context.startActivity(homepage);
+                }
+            }
+        });
+        return v;
+    }
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.invalidate();
+    }
+}
---
+++ INSTAGRAM/app/src/main/java/csejeonju2019/go/kr/insta/recommend7tobak.java
@@ -0,0 +1,122 @@
+package csejeonju2019.go.kr.insta;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.pm10.library.CircleIndicator;
+
+public class recommend7tobak extends AppCompatActivity
+        implements OnMapReadyCallback {
+
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.recommend7tobak);  //순서주의주의
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        Adapter7 adapter=new Adapter7(this);
+        ViewPager viewPager=(ViewPager)findViewById(R.id.rec7);
+        viewPager.setAdapter(adapter);
+
+
+        viewPager.setCurrentItem(0);
+
+        CircleIndicator circleIndicator1=(CircleIndicator)findViewById(R.id.circle_indicator);
+
+        circleIndicator1.setupWithViewPager(viewPager);
+    }
+
+
+    @Override
+    public void onMapReady(final GoogleMap googleMap) {
+
+
+        mMap = googleMap;
+        LatLng kyongki = new LatLng(35.81498, 127.14996);
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(kyongki);
+        markerOptions.title("경기전");
+
+        LatLng uhzin = new LatLng(35.8162, 127.1494);
+
+        MarkerOptions markerOptions2 = new MarkerOptions();
+        markerOptions2.position(uhzin);
+        markerOptions2.title("어진박물관");
+
+        LatLng taezo = new LatLng(35.81439, 127.15101);
+
+        MarkerOptions markerOptions3 = new MarkerOptions();
+        markerOptions3.position(taezo);
+        markerOptions3.title("태조로");
+
+
+        LatLng ohmok = new LatLng(35.81405, 127.1545);
+
+        MarkerOptions markerOptions4 = new MarkerOptions();
+        markerOptions4.position(ohmok);
+        markerOptions4.title("오목대");
+        LatLng eemok = new LatLng(35.81426, 127.15613);
+
+        MarkerOptions markerOptions5 = new MarkerOptions();
+        markerOptions5.position(eemok);
+        markerOptions5.title("이목대");
+        LatLng jaman = new LatLng(35.81421, 127.15721);
+
+        MarkerOptions markerOptions6 = new MarkerOptions();
+        markerOptions6.position(jaman);
+        markerOptions6.title("자만벽화마을");
+
+        mMap.addPolyline(new PolylineOptions().add(kyongki,uhzin).width(5).color(Color.RED));
+        mMap.addPolyline(new PolylineOptions().add(uhzin,taezo).width(5).color(Color.RED));
+        mMap.addPolyline(new PolylineOptions().add(taezo,ohmok).width(5).color(Color.RED));
+        mMap.addPolyline(new PolylineOptions().add(ohmok,eemok).width(5).color(Color.RED));
+        mMap.addPolyline(new PolylineOptions().add(eemok,jaman).width(5).color(Color.RED));
+        BitmapDrawable bitmapdraw=(BitmapDrawable)getResources().getDrawable(R.drawable.mappin_saram);
+        Bitmap b=bitmapdraw.getBitmap();
+        Bitmap smallMarker = Bitmap.createScaledBitmap(b, 75, 75, false);
+        BitmapDrawable bitmapdraw2=(BitmapDrawable)getResources().getDrawable(R.drawable.mappinstart);
+        Bitmap b2=bitmapdraw2.getBitmap();
+        Bitmap smallMarker2 = Bitmap.createScaledBitmap(b2, 80, 80, false);
+        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(smallMarker2));
+        markerOptions2.icon(BitmapDescriptorFactory.fromBitmap(smallMarker));
+        markerOptions3.icon(BitmapDescriptorFactory.fromBitmap(smallMarker));
+        markerOptions4.icon(BitmapDescriptorFactory.fromBitmap(smallMarker));
+        markerOptions5.icon(BitmapDescriptorFactory.fromBitmap(smallMarker));
+        markerOptions6.icon(BitmapDescriptorFactory.fromBitmap(smallMarker));
+
+        mMap.addMarker(markerOptions);
+        mMap.addMarker(markerOptions2);
+        mMap.addMarker(markerOptions3);
+        mMap.addMarker(markerOptions4);
+        mMap.addMarker(markerOptions5);
+        mMap.addMarker(markerOptions6);
+        LatLng center=new LatLng(35.81474, 127.1526);
+
+
+
+
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(center, 16));//16배 줌가능
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(center, 15));//지도 시작시 애니메이션 효과
+    }
+}
